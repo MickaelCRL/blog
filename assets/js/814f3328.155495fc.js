c69b48d58456f95a1db995a2f60185bff6f4ec30
MickaelCRL <ce.mickael27@gmail.com>
2023-09-29 23:57:00 +0200

    Deploy website - based on 4192af69272802b484d7d61dab8eec7d6550ed5b

---
+++ assets/js/814f3328.155495fc.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmickael_blog=self.webpackChunkmickael_blog||[]).push([[535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"first-blog-post","permalink":"/blog/2019/05/28/first-blog-post"}]}')}}]);
