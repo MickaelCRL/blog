88bac76816b363c7214b2a8fe83b085f5bff9872
MickaelCRL <ce.mickael27@gmail.com>
2024-03-07 20:57:46 +0100

    Deploy website - based on 2fa45ea8df9678b0e0a4f293ba47eba11ad570b4

--- assets/js/814f3328.7d1968eb.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmickael_blog=self.webpackChunkmickael_blog||[]).push([[535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Pr\xe9sentation RentCRL","permalink":"/blog/presentation-rentcrl"}]}')}}]);
